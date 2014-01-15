9a991e9f275bd940dedf490545f20a73393cc87a
Chris Golaszewski <chrisg@oatmeal.org>
2014-01-15 14:32:17 -0800

    Initial commit of public website.

---
+++ app/js/app.js
@@ -0,0 +1,43 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+
+
+$(function() {
+
+	var navHeightOffset = $('.top-bar').height() + 10;
+
+	$('.dig-in-shared-success').hide();
+	$('.btn-dig-in').show(100);
+
+	$('.btn-dig-in a').on('click', function(e) {
+		e.preventDefault();
+		var scrollTarget = $('.btn-dig-in').first().offset().top - navHeightOffset;
+		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
+			// Note, might need browser detection to run animate() on html rather than body for IE8
+			$('.dig-in-shared-success').slideToggle(300, function() {
+				$('.btn-dig-in a').html(($('.btn-dig-in').first().find('a').html() == 'Show More') ? 'Show Less' : 'Show More');
+			});
+		});
+	});
+
+	$('.btn-model-shared-success a').on('click', function(e) {
+		e.preventDefault();
+		var scrollTarget = $('.detail-shared-success h1').first().offset().top - navHeightOffset;
+		$('body').animate({ scrollTop: scrollTarget }, 300);
+	});
+
+	$('.btn-open-positions a').on('click', function(e) {
+		e.preventDefault();
+		var scrollTarget = $('.detail-open-positions h1').first().offset().top - navHeightOffset;
+		$('body').animate({ scrollTop: scrollTarget }, 300);
+	});
+
+	$('.btn-contact a').on('click', function(e) {
+		e.preventDefault();
+		var scrollTarget = $('footer').offset().top - navHeightOffset;
+		$('body').animate({ scrollTop: scrollTarget }, 300);
+	});
+
+});
+
