485fd7f53576aae9fd97bbe730e3a7fe258f170c
Chris Golaszewski <chrisg@oatmeal.org>
2014-01-21 20:04:59 -0800

    Add jQueryUI for easing fanciness
    Added jQueryUI — specifically, only the effect module. Updated app to
    use newest Foundation which fixes the broken sticky_menu.

--- app/js/app.js
+++ app/js/app.js
@@ -24,37 +24,41 @@ $(function() {
 	$('.link-to-recruiting-process').on('click', function(e) {
 		e.preventDefault();
 		var scrollTarget = $('.detail-recruiting-process h1').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300);
+		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
+			$('body').animate({ scrollTop: scrollTarget - 50 }, 100, function() {
+				$('body').animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
+			});
+		});
 	});
 
 	$('.link-to-model-shared-success').on('click', function(e) {
 		e.preventDefault();
 		var scrollTarget = $('.detail-shared-success h1').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300);
+		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
+			$('body').animate({ scrollTop: scrollTarget - 50 }, 100, function() {
+				$('body').animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
+			});
+		});
 	});
 
 	$('.link-to-virtuous-cycle').on('click', function(e) {
 		e.preventDefault();
 		var scrollTarget = $('.detail-virtuous-cycle h1').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300);
+		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
+			$('body').animate({ scrollTop: scrollTarget - 50 }, 100, function() {
+				$('body').animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
+			});
+		});
 	});
 
-	$('.btn-open-positions a').on('click', function(e) {
+	$('.link-to-service-offering').on('click', function(e) {
 		e.preventDefault();
-		var scrollTarget = $('.detail-open-positions h1').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300);
-	});
-
-	// $('.btn-contact a').on('click', function(e) {
-	// 	e.preventDefault();
-	// 	var scrollTarget = $('footer').offset().top - navHeightOffset;
-	// 	$('body').animate({ scrollTop: scrollTarget }, 300);
-	// });
-
-	$('.btn-services a').on('click', function(e) {
-		e.preventDefault();
-		var scrollTarget = $('.detail-service-offering h1').offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300);
+		var scrollTarget = $('.detail-service-offering h1').first().offset().top - navHeightOffset;
+		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
+			$('body').animate({ scrollTop: scrollTarget - 50 }, 100, function() {
+				$('body').animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
+			});
+		});
 	});
 
 });
