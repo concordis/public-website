533d31462f930acf02a9d2f90988e1dbc08cee85
Chris Golaszewski <chrisg@oatmeal.org>
2014-03-11 16:31:55 -0700

    Fixed broken links in FF and maybe IE
    Modernizr hack to conditionally call link and .animate functions on
    body versus html element.

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,8 @@ $(document).foundation();
 
 $(function() {
 
+	// I do not know if testing for flexbox is the best way to test browser but it seems to work.
+	var bodyOrHTML = (Modernizr.flexbox) ? 'body' : 'html';
 	var navHeightOffset = $('.top-bar').height() + 10;
 
 	$('.dig-in-shared-success').hide();
@@ -13,21 +15,20 @@ $(function() {
 	$('.btn-dig-in a').on('click', function(e) {
 		e.preventDefault();
 		var scrollTarget = $('.btn-dig-in').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
-			// Note, might need browser detection to run animate() on html rather than body for IE8
+		$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, function() {
 			$('.dig-in-shared-success').slideToggle(300, function() {
 				$('.btn-dig-in a').html(($('.btn-dig-in').first().find('a').html() == 'Show More') ? 'Show Less' : 'Show More');
 			});
 		});
 	});
 
-	// All these .link-to- functions should be rewritten to a single vriable function.
+	// All these .link-to- functions should be rewritten to a single variable function.
 	$('.link-to-concordis-way').on('click', function(e) {
 		e.preventDefault();
 		var scrollTarget = $('.detail-concordis-way h1').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
-			$('body').animate({ scrollTop: scrollTarget - 50 }, 100, function() {
-				$('body').animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
+		$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, function() {
+			$(bodyOrHTML).animate({ scrollTop: scrollTarget - 50 }, 100, function() {
+				$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
 			});
 		});
 	});
@@ -35,9 +36,9 @@ $(function() {
 	$('.link-to-recruiting-process').on('click', function(e) {
 		e.preventDefault();
 		var scrollTarget = $('.detail-recruiting-process h1').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
-			$('body').animate({ scrollTop: scrollTarget - 50 }, 100, function() {
-				$('body').animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
+		$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, function() {
+			$(bodyOrHTML).animate({ scrollTop: scrollTarget - 50 }, 100, function() {
+				$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
 			});
 		});
 	});
@@ -45,9 +46,9 @@ $(function() {
 	$('.link-to-model-shared-success').on('click', function(e) {
 		e.preventDefault();
 		var scrollTarget = $('.detail-shared-success h1').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
-			$('body').animate({ scrollTop: scrollTarget - 50 }, 100, function() {
-				$('body').animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
+		$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, function() {
+			$(bodyOrHTML).animate({ scrollTop: scrollTarget - 50 }, 100, function() {
+				$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
 			});
 		});
 	});
@@ -55,9 +56,9 @@ $(function() {
 	$('.link-to-virtuous-cycle').on('click', function(e) {
 		e.preventDefault();
 		var scrollTarget = $('.detail-virtuous-cycle h1').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
-			$('body').animate({ scrollTop: scrollTarget - 50 }, 100, function() {
-				$('body').animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
+		$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, function() {
+			$(bodyOrHTML).animate({ scrollTop: scrollTarget - 50 }, 100, function() {
+				$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
 			});
 		});
 	});
@@ -65,9 +66,9 @@ $(function() {
 	$('.link-to-industry-secrets').on('click', function(e) {
 		e.preventDefault();
 		var scrollTarget = $('.detail-industry-secrets h1').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
-			$('body').animate({ scrollTop: scrollTarget - 50 }, 100, function() {
-				$('body').animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
+		$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, function() {
+			$(bodyOrHTML).animate({ scrollTop: scrollTarget - 50 }, 100, function() {
+				$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
 			});
 		});
 	});
@@ -75,9 +76,9 @@ $(function() {
 	$('.link-to-service-offering').on('click', function(e) {
 		e.preventDefault();
 		var scrollTarget = $('.detail-service-offering h1').first().offset().top - navHeightOffset;
-		$('body').animate({ scrollTop: scrollTarget }, 300, function() {
-			$('body').animate({ scrollTop: scrollTarget - 50 }, 100, function() {
-				$('body').animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
+		$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, function() {
+			$(bodyOrHTML).animate({ scrollTop: scrollTarget - 50 }, 100, function() {
+				$(bodyOrHTML).animate({ scrollTop: scrollTarget }, 300, 'easeInBack');
 			});
 		});
 	});
