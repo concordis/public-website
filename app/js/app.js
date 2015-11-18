2fb89858e3da827870b880756124f983547207fe
Chris Golaszewski <chrisg@oatmeal.org>
2015-11-18 15:16:00 -0800

    Added rate calculator to homepage
    In addition to replacing the Shared Success section with the Rate
    Calculator, I also made a few subtle changes to the hero video and
    removed some unused scripting from JS.

--- app/js/app.js
+++ app/js/app.js
@@ -5,21 +5,6 @@ $(document).foundation();
 
 $(function() {
 
-  $('.dig-in-shared-success').hide();
-  $('.btn-dig-in').show(100);
-
-  $('.btn-dig-in a').on('click', function(e) {
-    e.preventDefault();
-    var scrollTarget = $('.btn-dig-in').first().offset().top - $('nav').height();
-    // Note this does not animate in Firefox.
-    // If we place html,body in the selector then it does not work properly in all other browsers.
-    $('body').stop().animate({ scrollTop: scrollTarget }, 300, function() {
-      $('.dig-in-shared-success').slideToggle(300, function() {
-        $('.btn-dig-in a').html(($('.btn-dig-in').first().find('a').html() == 'Show More') ? 'Show Less' : 'Show More');
-      });
-    });
-  });
-
   $('.link-to').on('click', function(e) {
     e.preventDefault();
     var navHeightOffset = $('nav').height();
